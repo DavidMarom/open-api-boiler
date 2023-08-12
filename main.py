43ffa4d42ef4d4865cfd431c70a98e37c2f39bca
David Marom <davidmarom.pro@gmail.com>
2023-08-12 14:28:38 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import connexion
+
+if __name__ == '__main__':
+    print(f'Starting server')
+    try:
+        app = connexion.App(__name__, specification_dir='./')
+        file_path = os.path.dirname(__file__)
+        app.add_api(file_path + '/open_api.yaml')
+        app.run(port=8080)
+    except Exception as e:
+        print(f'Error: {e}')
