3303b149479af5e4b6647888523f713fbd422b94
David Marom <davidmarom.pro@gmail.com>
2023-08-19 16:15:21 +0300

    added hard coded DB

--- functions/functions.py
+++ functions/functions.py
@@ -1,3 +1,18 @@
-def say_hi():
-    print("Hi!")
-    return "Hi!"
+db = [
+    {
+        "name": "John",
+        "age": 30
+    },
+    {
+        "name": "Jane",
+        "age": 25
+    },
+]
+
+
+def say_hello():
+    return "This is the API for the api boiler app"
+
+
+def get_users():
+    return db
