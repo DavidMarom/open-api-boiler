0d512ec4d4cee5d8ff7a880ed631f4b95b991708
David Marom <davidmarom.pro@gmail.com>
2023-08-26 09:42:15 +0300

    added routes

--- functions/functions.py
+++ functions/functions.py
@@ -1,13 +1,3 @@
-db2 = [
-    {
-        "name": "John",
-        "age": 30
-    },
-    {
-        "name": "Jane",
-        "age": 25
-    },
-]
 import json
 import os
 from supabase import create_client, Client
@@ -19,13 +9,26 @@ key: str = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(url, key)
 
-response = supabase.table('users').select("*").execute()
-
-
 
 def say_hello():
     return "This is the API for the api boiler app"
 
 
 def get_users():
-    return response.data
+    return supabase.table('users').select("*").execute().data
+
+
+def get_user_by_id(id):
+    return supabase.table('users').select("*").eq('id', id).execute().data
+
+
+def create_user(body):
+    return supabase.table('users').insert(body).execute().data
+
+
+def update_user(id, body):
+    return supabase.table('users').update(body).eq('id', id).execute().data
+
+
+def delete_user(id):
+    return supabase.table('users').delete().eq('id', id).execute().data
